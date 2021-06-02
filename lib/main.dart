46f5acadd320cb2a784791d224b3914dc6daf91b
moon1lightx <ainur.is1701@gmail.com>
2021-06-02 21:35:42 +0600

    nav to other sreens

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavigationDrawerWidget(),
-    appBar: AppBar(title: Text("Campus IITU"),),
+    appBar: AppBar(title: Text("Campus IITU"), backgroundColor: Color(0xFF0F1F3D)),
     );
   }
 }
@@ -35,7 +35,7 @@ class NavigationDrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
+      child: Material(
         color: Color(0xFF0F1F3D),
         child: ListView(
           children: [
@@ -43,23 +43,139 @@ class NavigationDrawerWidget extends StatelessWidget {
               Container(
                 margin: EdgeInsets.all(20),
                 child: Text('CAMPUS IITU', style: TextStyle(color: Colors.white, fontSize: 25))),
-              buildMenuItem( "Profile", Icons.person),
-              buildMenuItem( "Schedule", Icons.calendar_today),
-              buildMenuItem( "My Study Plan", Icons.list_alt_outlined),
-              buildMenuItem( "Certificates", Icons.dock_outlined),
-              buildMenuItem( "Log out", Icons.logout),
+              buildMenuItem( "Profile", Icons.person,  ()  => selectedItem(context, 0)),
+              buildMenuItem( "Notifications", Icons.notification_important, ()  => selectedItem(context, 1)),
+              buildMenuItem( "Schedule", Icons.calendar_today, ()  => selectedItem(context, 2)),
+              buildMenuItem( "My Study Plan", Icons.list_alt_outlined, ()  => selectedItem(context, 3)),
+              buildMenuItem( "Certificates", Icons.dock_outlined, ()  => selectedItem(context, 4)),
+              buildMenuItem( "Log out", Icons.logout, ()  => selectedItem(context, 5)),
           ],
         ),
       )
     );
   }
 
-  Widget buildMenuItem(String text, IconData icon) {
+  Widget buildMenuItem(String text, IconData icon, VoidCallback onClicked) {
     return ListTile(
       leading: Icon(icon, color: Colors.white),
       title: Text(text, style: TextStyle(color: Colors.white)),
       hoverColor: Colors.white70,
-      onTap: (){},
+      onTap: onClicked,
+    );
+  }
+
+  void selectedItem(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context)  => ProfilePage(),
+          ));
+          break;
+         case 1:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context)  => NotificationPage(),
+          ));
+          break;
+         case 2:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context)  => SchedulePage(),
+          ));
+          break;
+         case 3:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context)  => StudyPlanPage(),
+          ));
+          break;
+           case 4:
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context)  => CertificatePage(),
+          ));
+          break;
+    }
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
     );
   }
 }
+
+class AcademicCalendarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Academic Calendar'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
+    );
+  }
+}
+
+class NotificationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
+    );
+  }
+}
+
+class SchedulePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Schedule'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
+    );
+  }
+}
+
+
+class StudyPlanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Study Plan'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
+    );
+  }
+}
+
+
+
+class CertificatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Certificates'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF0F1F3D)
+      ),
+    );
+  }
+}
+
+
+
+
